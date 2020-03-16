fbf8f640594754d3ecab0b84141dc1f7c6591179
杨康 <1600163828@qq.com>
2020-03-16 20:37:39 +0800

    3.16提交

--- main.c
+++ main.c
@@ -59,8 +59,18 @@ int Length_LinkList(LinkList H){
     return j;
 }
 
-//按序号查找
-int
+//单链表的逆置
+void Reverse(LinkList H){
+    LNode *q,*p;
+    p=H->next;
+    H->next=NULL;
+    while(p) {
+        q=p;
+        p=p->next;
+        q->next=H->next;
+        H->next=q;
+    }
+}
 
 
 
