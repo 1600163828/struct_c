bc427b77e2f61d35d4440683963ce29d0b653f65
杨康 <1600163828@qq.com>
2020-03-20 12:36:40 +0800

    3.20练习

--- main.c
+++ main.c
@@ -1,3 +1,231 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+typedef struct LNode *PtrToLNode;
+struct LNode {
+    ElementType Data;
+    PtrToLNode Next;
+};
+typedef PtrToLNode List;
+
+List Read(); /* 细节在此不表 */
+
+int Length( List L );
+
+int main()
+{
+    List L = Read();
+    printf("%d\n", Length(L));
+    return 0;
+}
+int Length( List L ){
+
+    int i=0;
+    while(L->Next!=NULL){
+        i++;
+    }
+    return i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//struct ListNode {
+//    int data;
+//    struct ListNode *next;
+//};
+//
+//struct ListNode *createlist();
+//
+//int main()
+//{
+//    struct ListNode *p, *head = NULL;
+//
+//    head = createlist();
+//    for ( p = head; p != NULL; p = p->next )
+//        printf("%d ", p->data);
+//    printf("\n");
+//
+//    return 0;
+//}
+//
+//struct ListNode *createlist(){
+//
+//
+//    struct ListNode *p,*H=NULL;
+//    H=(struct ListNode*)(malloc(sizeof(struct ListNode)));
+//    H->next=NULL;
+//    int x;
+//    scanf("%d",&x);
+//    while (x!=-1){
+//        p=(struct ListNode*)(malloc(sizeof(struct ListNode)));
+//        p->data=x;
+//        p->next=H->next;
+//        H->next=p;
+//        scanf("%d",&x);
+//    }
+//    return H->next;
+//
+//}
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//#define MAXSIZE 5
+//#define ERROR -1
+//typedef enum {false, true} bool;
+//typedef int ElementType;
+//typedef int Position;
+//typedef struct LNode *List;
+//struct LNode {
+//    ElementType Data[MAXSIZE];
+//    Position Last; /* 保存线性表中最后一个元素的位置 */
+//};
+//
+//
+//List MakeEmpty();//创建并返回一个空的线性表；
+//Position Find( List L, ElementType X );//返回线性表中X的位置。若找不到则返回ERROR；
+//bool Insert( List L, ElementType X, Position P );//将X插入在位置P并返回true。若空间已满，则打印“FULL”并返回false；如果参数P指向非法位置，则打印“ILLEGAL POSITION”并返回false
+//bool Delete( List L, Position P );//将位置P的元素删除并返回true。若参数P指向非法位置，则打印“POSITION P EMPTY”（其中P是参数值）并返回false
+//
+//List MakeEmpty(){//创建并返回一个空的线性表
+//    List L;
+//    L=(List)malloc(sizeof(struct LNode));
+//    L->Last=(-1);
+//    return L;
+//}
+//
+//Position Find( List L, ElementType X ){
+//
+//    if(L->Last==(-1)){
+//        return ERROR;
+//    } else{
+//        int i=0;
+//        while(L->Data[i] != X && i<MAXSIZE-1){
+//            i++;
+//        }
+//        if(i==MAXSIZE-1 && L->Data[i] != X ){
+//            return ERROR;
+//        } else return i;
+//
+//    }
+//}
+//
+//bool Insert( List L, ElementType X, Position P ){
+//    if(P>=MAXSIZE-1){
+//        printf("ILLEGAL POSITION");
+//        return false;
+//    }
+//    if(L->Last==MAXSIZE-1){
+//        printf("FULL");
+//        return false;
+//    }
+//    for (int j = L->Last; j >= P; j--) {
+//        L->Data[j+1]=L->Data[j];
+//    }
+//    L->Data[P]=X;
+//    L->Last++;
+//}
+//
+//bool Delete( List L, Position P ){
+//    if(P>=MAXSIZE-1 || L->Last > MAXSIZE-1){
+//        printf("POSITION %d EMPTY",P);
+//        return false;
+//    } else{
+//        return true;
+//    }
+//
+//}
+//
+//
+//int main()
+//{
+//    List L;
+//    ElementType X;
+//    Position P;
+//    int N;
+//
+//    L = MakeEmpty();
+//    scanf("%d", &N);
+//    while ( N-- ) {
+//        scanf("%d", &X);
+//        if ( Insert(L, X, 0)==false )
+//            printf(" Insertion Error: %d is not in.\n", X);
+//    }
+//    scanf("%d", &N);
+//    while ( N-- ) {
+//        scanf("%d", &X);
+//        P = Find(L, X);
+//        if ( P == ERROR )
+//            printf("Finding Error: %d is not in.\n", X);
+//        else
+//            printf("%d is at position %d.\n", X, P);
+//    }
+//    scanf("%d", &N);
+//    while ( N-- ) {
+//        scanf("%d", &P);
+//        if ( Delete(L, P)==false )
+//            printf(" Deletion Error.\n");
+//        if ( Insert(L, 0, P)==false )
+//            printf(" Insertion Error: 0 is not in.\n");
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //顺序栈
 #include <stdio.h>
 #include <stdlib.h>
@@ -91,20 +319,7 @@ void SetEmpty(SeqStack *s){
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
