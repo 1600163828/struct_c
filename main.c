a4cb2bf3f067f56cba9e3fa29b7304f41d8b20d3
杨康 <1600163828@qq.com>
2020-03-22 21:02:52 +0800

    3.22练习

--- main.c
+++ main.c
@@ -1,32 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-typedef int ElementType;
-typedef struct LNode *PtrToLNode;
-struct LNode {
-    ElementType Data;
-    PtrToLNode Next;
-};
-typedef PtrToLNode List;
 
-List Read(); /* 细节在此不表 */
-
-int Length( List L );
-
-int main()
-{
-    List L = Read();
-    printf("%d\n", Length(L));
-    return 0;
-}
-int Length( List L ){
-
-    int i=0;
-    while(L->Next!=NULL){
-        i++;
-    }
-    return i;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//typedef int ElementType;
+//typedef struct LNode *PtrToLNode;
+//struct LNode {
+//    ElementType Data;
+//    PtrToLNode Next;
+//};
+//typedef PtrToLNode List;
+//
+//List Read(); /* 细节在此不表 */
+//
+//int Length( List L );
+//
+//int main()
+//{
+//    List L = Read();
+//    printf("%d\n", Length(L));
+//    return 0;
+//}
+//int Length( List L ){
+//
+//    int i=0;
+//    while(L->Next!=NULL){
+//        i++;
+//    }
+//    return i;
+//}
 
 
 
