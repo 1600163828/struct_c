3ad6e42486b4bea7bc73b43aad35e4db3f0e9c3f
杨康 <1600163828@qq.com>
2020-06-30 11:48:40 +0800

    'ni'

--- main.c
+++ main.c
@@ -4,6 +4,14 @@ int main(){
 
     // 已知一棵二叉树先序序列为：A B C，中序序列为：B A C，请回复改二叉树并写出后序序列。(3分)
 
+    //sfvfdv
+
+    int i=0;
+    while(i<8){
+        printf("%d\n",i);
+        i++;
+        
+    }
     printf("hello");
     return 0;
 }
